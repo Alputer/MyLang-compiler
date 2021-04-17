963cceaf2d2210c5f2cdc43b44e457f68199adad
Alp Tuna <alp.tuna.453@gmail.com>
2021-04-17 19:59:57 +0300

    same as curr.cpp

--- MyCode2.cpp
+++ MyCode2.cpp
@@ -8,15 +8,17 @@
 using namespace std;
 
 string expr(string text);
+bool assignmentForChoose(string var_name, string text);
 
 ///////////////////////////////////These variables are commonly used in most of the below functions
 
 ofstream outfile;
 int tmp_counter = 1;  //Used for creation of temporary variables.
+int cnd_counter = 1;  //Used for creation of conditional variables.
 int while_if_counter = 1; // Used for creation of while/if bodies.
 bool is_in_while = false; //Used for discriminating between while and if bodies, keeps this information in memory.
 unordered_set<string> variables; // all variable names are put here.
-
+unordered_set<string> cond_variables; // all conditional variable names are put here.
 
 
 
@@ -25,9 +27,29 @@ string createTmpVariable(){
 	return "%_" + to_string(tmp_counter++);
 }
 
+string createCndVariable(){
+  return "v" + to_string(cnd_counter++); // This will not be a temporary variable.
+}
 
 ////////////////////////////// These functions writes to out file
+      
+void llComp(string result, string var1){
+    outfile << "    " << result <<  " = icmp eq i32 " <<  var1 << ", 0" << endl;
+}
 
+// reutrns (var1 > var2)
+void llGreater(string result, string var1){
+    outfile << "    " << result <<  " = icmp sgt i32 " <<  var1 << ", 0" << endl;
+}
+
+void llLess(string result, string var1){
+    outfile << "    " << result <<  " = icmp slt i32 " <<  var1 << ", 0" << endl;
+}
+
+// Does type casting to var_bool.
+void llTypeCast(string result, string var_bool){
+    outfile << "    " << result << " = zext i1 " << var_bool << " to i32" << endl;
+}
 void llAlloca(string var_name){
 	outfile << "    " << var_name << " = alloca i32" << endl;
 }
@@ -116,7 +138,35 @@ void deleteComment(string& s){
       	if(s[i] == '#')   s = s.substr(0, i);
 	}
 }
- 
+
+//Returns the expressions in the choose function. expr1,expr2,expr3 and expr4.
+//Nested choose fonksiyonlarını yine parantezle kontrol edebiliyoruz.
+vector<string> findExpressions(string text){
+  
+  int brace1 = text.find("(");
+  int brace2 = text.find_last_of(")");
+  vector<int> commaIndexes; //Index of the three commas.
+
+  int bracenum1 = 0;
+  int bracenum2 = 0;
+  for(int i=brace1+1; i<text.size(); i++){
+     if(text[i] == '(')
+      bracenum1++;
+     if(text[i] == ')')
+      bracenum2++;
+     if(text[i] == ',' && bracenum1 == bracenum2)
+       commaIndexes.push_back(i);
+  }
+
+    vector<string> result;
+    result.push_back(text.substr(brace1 + 1, commaIndexes[0] - brace1 - 1));
+    result.push_back(text.substr(commaIndexes[0] + 1, commaIndexes[1] - commaIndexes[0] - 1));
+    result.push_back(text.substr(commaIndexes[1] + 1, commaIndexes[2] - commaIndexes[1] - 1));
+    result.push_back(text.substr(commaIndexes[2] + 1, brace2 - commaIndexes[2] - 1));
+
+    return result;
+ }
+
        //Parantheses should match, hence we need such a function. We cannot arbitrarily choose the last sign.
        //Returns the index of the first available sign.
 int findLastAvailableAddSub(string s){
@@ -160,13 +210,60 @@ int findLastAvailableMultDiv(string s){
 
 string factor(string text){
   	int value;
+    // it is a decimal number.
   	if(stringstream(text) >> value){
-    	// it is a decimal number.
     	return text;
 	}
 	//It is something with parantheses. Handles the parantheses part.
     else if(text[0] == '(' && text[text.size() - 1] == ')'){
 		return expr(text.substr(1,text.size() - 2)); // (expr) -> expr
+    }
+
+    else if(text.find("choose") != string::npos){
+      
+      vector<string> expressions = findExpressions(text); // Su anda dogru calisiyor.
+      string expr0 = expressions[0];
+      string expr1 = expressions[1];
+      string expr2 = expressions[2];
+      string expr3 = expressions[3];
+
+
+
+       string s = expr(expr0); // s is the name of the variable which keeps the result of the expr1.
+       //cout << s;
+      string cond1 = createCndVariable(); // They will be the variables v1, v2 etc..
+      string cond2 = createCndVariable(); // Which keeps the boolean information as a i1 variable.
+      string cond3 = createCndVariable();
+      string num1 = createCndVariable(); // They will be the variables v1, v2 etc..
+      string num2 = createCndVariable(); // Which keeps the boolean information as a i32 variable.
+      string num3 = createCndVariable();
+      
+      //variables.insert(cond1); v1
+      //variables.insert(cond2); v2
+      //variables.insert(cond3); v3
+      variables.insert(num1); //v4
+      variables.insert(num2); //v5
+      variables.insert(num3); //v6
+      cond_variables.insert(num1);
+      cond_variables.insert(num2);
+      cond_variables.insert(num3);
+
+      //llAlloca("%" + cond1); Not necessary!!
+      //llAlloca("%"+ cond2);
+      //llAlloca("%" + cond3);
+
+
+      llComp("%" + cond1, s);  // result1 is the boolean variable which keeps the information.
+      llGreater("%" + cond2, s);
+      llLess("%" + cond3, s);  
+      
+      llTypeCast("%" + num1, "%" + cond1);
+      llTypeCast("%" + num2, "%" + cond2);
+      llTypeCast("%" + num3, "%" + cond3);
+
+      string result = "" + num1 + "*(" + expr1 + ")+" + num2 + "*(" + expr2 + ")+" + num3 + "*(" + expr3 + ")";
+      cout << result << endl;
+      return expr(result);
     }
   	else{
     // Here I assumed that variable exists and it is already declared. We should improve this part later!!!
@@ -176,10 +273,15 @@ string factor(string text){
          	llStore("%" + text, "0");
          	variables.insert(text);
     	}
+      // It means it is not a conditional variable.
+      if(cond_variables.find(text) == cond_variables.end()){
 		string tmp = createTmpVariable();
 		llLoad("%" + text, tmp);
 		return tmp;
   }
+  return "%" + text; //It returns if it is a conditional variable.
+  }
+
 }
 
 
@@ -209,7 +311,6 @@ string term(string text){
 string expr(string text){
     //int pos = text.find_last_of("+-");
 	int pos = findLastAvailableAddSub(text);
-      
       //Not found
     if(pos == -1){
       	return term(text);
@@ -230,12 +331,12 @@ string expr(string text){
 
 // var_name is without %. Returns false if there is a syntax error
 bool assignment(string var_name, string text){
+
 	if (variables.find(var_name) == variables.end()){
 		// this variable hasn't been declared before
-		llAlloca("%" + var_name);
+    llAlloca("%" + var_name);
 		variables.insert(var_name);
 	}
-
 	string value = expr(text);
 	llStore("%" + var_name, value);
 
@@ -249,11 +350,26 @@ bool print(string text){
 	return true;
 }
 
+bool assignmentForChoose(string var_name, string text){
+   if (variables.find(var_name) == variables.end()){
+    // this variable hasn't been declared before
+    llAlloca("%" + var_name);
+    variables.insert(var_name);
+  }
+
+  string value = expr(text);
+  llStore("%" + var_name, value);
+
+  return true;
+}
+
 bool handleWhileCondition(string text){
 	string value = expr(text); // %t1
 	string result = createTmpVariable();
 	outfile << "    " << result << " = icmp ne i32 " << value << ", 0" << endl;
 	outfile << "    br i1 "<< result << ", label %body" << while_if_counter <<", label %end" << while_if_counter << endl << endl;
+
+  return true;
 }
 
 
@@ -266,6 +382,8 @@ bool condition(string text){
 	outfile << "cond" << while_if_counter << ":" << endl;
 	handleWhileCondition(expression);
 	outfile << "body" << while_if_counter << ":" << endl;
+
+  return true;
 }
 
 
@@ -273,7 +391,10 @@ bool condition(string text){
 int main(){
 	// checksyntax();
 
-
+   //string dsc = "choose(a,b,c,d)";
+   
+  // vector<string> v = findExpressions(dsc);
+   //cout << v[0] << " " << v[1]<< " " << v[2] << " " << v[3];
 	//This is the input file.
 	ifstream infile;
 	infile.open("main.my");
